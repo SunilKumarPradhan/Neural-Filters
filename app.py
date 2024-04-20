b56285f8e3273a318c63b8725ad83407fc470418
Sunil Kumar Pradhan <sunilkumarweb47@gmail.com>
2024-04-20 15:11:23 +0530

    Added all required files

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+from data import *
+from input import image_input, webcam_input
+
+st.title("Realtime : Filters Based on Neural Networks ")
+st.sidebar.title('Navigation')
+method = st.sidebar.radio('Go To ->', options=['Webcam', 'Image'])
+st.sidebar.header('Options')
+
+style_model_name = st.sidebar.selectbox("Choose the style model: ", style_models_name)
+
+if method == 'Image':
+    image_input(style_model_name)
+else:
+    webcam_input(style_model_name)
---
+++ input.py
@@ -0,0 +1,73 @@
+import numpy as np
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+from PIL import Image
+import cv2
+import imutils
+from neural_style_transfer import get_model_from_path, style_transfer
+from data import *
+
+import av
+from turn import get_ice_servers
+from streamlit_session_memo import st_session_memo
+
+def image_input(style_model_name):
+    style_model_path = style_models_dict[style_model_name]
+
+    model = get_model_from_path(style_model_path)
+
+    if st.sidebar.checkbox('Upload'):
+        content_file = st.sidebar.file_uploader("Choose a Content Image", type=["png", "jpg", "jpeg"])
+    else:
+        content_name = st.sidebar.selectbox("Choose the content images:", content_images_name)
+        content_file = content_images_dict[content_name]
+
+    if content_file is not None:
+        content = Image.open(content_file)
+        content = np.array(content) #pil to cv
+        content = cv2.cvtColor(content, cv2.COLOR_RGB2BGR)
+    else:
+        st.warning("Upload an Image OR Untick the Upload Button)")
+        st.stop()
+
+    WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)), value=200)
+    content = imutils.resize(content, width=WIDTH)
+    generated = style_transfer(content, model)
+    st.sidebar.image(content, width=300, channels='BGR')
+    st.image(generated, channels='BGR', clamp=True)
+
+
+def webcam_input(style_model_name):
+    st.header("Webcam Live Feed")
+    WIDTH = st.sidebar.select_slider('QUALITY (May reduce the speed)', list(range(150, 501, 50)))
+    width = WIDTH
+
+    @st_session_memo
+    def load_model(model_name, width):  # `width` is not used when loading the model, but is necessary as a cache key.
+        return get_model_from_path(model_name)
+
+    model = load_model(style_models_dict[style_model_name], width)
+
+    def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+        image = frame.to_ndarray(format="bgr24")
+
+        if model is None:
+            return image
+
+        orig_h, orig_w = image.shape[0:2]
+
+        # cv2.resize used in a forked thread may cause memory leaks
+        input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
+
+        transferred = style_transfer(input, model)
+
+        result = Image.fromarray((transferred * 255).astype(np.uint8))
+        image = np.asarray(result.resize((orig_w, orig_h)))
+        return av.VideoFrame.from_ndarray(image, format="bgr24")
+
+    ctx = webrtc_streamer(
+        key="neural-style-transfer",
+        video_frame_callback=video_frame_callback,
+        rtc_configuration={"iceServers": get_ice_servers()},
+        media_stream_constraints={"video": True, "audio": False},
+    )
